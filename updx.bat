e190dbbe176abaf733fd18f9831893768bdb2aeb
kyadet <ryuichi@furthersystem.com>
2022-10-15 21:30:24 +0900

    rename, add mac platform

--- updx.bat
+++ updx.bat
@@ -10,14 +10,12 @@ set CSHARP_COMPLIER_PATH="%WINDIR%\Microsoft.NET\Framework\v4.0.30319"
 set GENERATE_ASSEMBLY=generate.exe
 set SOURCE=src
 set BINARY=bin
+set RESOURCE=res
 set PATH=%CSHARP_COMPLIER_PATH%;%PATH%
 set DEBUG=
-
 %DEBUG% del /s/q %GENERATE_ASSEMBLY%
-
-%DEBUG% csc /out:%GENERATE_ASSEMBLY% %SOURCE%\updx.cs %SOURCE%\MiniJSON.cs 
-%DEBUG% %GENERATE_ASSEMBLY% updx.json %BINARY% res
-
+%DEBUG% csc /out:%GENERATE_ASSEMBLY% %SOURCE%\generator.cs %SOURCE%\MiniJSON.cs 
+%DEBUG% %GENERATE_ASSEMBLY% %BINARY% %RESOURCE% settings.json
 %DEBUG% del /s/q %GENERATE_ASSEMBLY%
 
 pause
---
+++ updx.mac.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+swift src/generater.swift res settings.json
+# swiftc -o bin/updater updater.swift
